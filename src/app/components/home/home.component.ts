9fb305ec5f87c6ed266eaa8232a4d8d6b35a9f60
Thayna-Santana <oliveirathayna763@gmail.com>
2022-10-06 22:08:58 -0300

    Trabalhando com variáveis

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  public nomeProduto: string = 'Curso de Angular'; //criando variável
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;//concatenação literal string
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
+
+  constructor() {
+    //this.anuncio = 'O' + this.nomeProduto + 'está em promoção' //concatenação normal
+    console.log(`Nome do Produto: `,this.nomeProduto);
+    console.log(`anuncio: `,this.anuncio);
+    console.log(`Id: `,this.idProduto);
+    console.log(`Preço: `, this.precoProduto);
+    console.log(`Promoção: `, this.promocao);
+    var variavel1;
+    let variavel2;
+    const variavel3: number =10;
+
+    /*var idade = 19;
+    console.log('Minha idade é: ', idade);
+
+    let idadelet = 20;
+    console.log('Minha idade é: ', idadelet);
+
+   function imprimeIdade(){
+      var idade = 20;
+      console.log('Sua idade é :' idade);
+   }
+   */
+
+    function imprimeIdadeFor(){
+      for(var idade = 30; idade <=40; idade++){
+        console.log ('Idade dentro do for: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+   // imprimeIdade();
+    imprimeIdadeFor();
+  }
 
   ngOnInit(): void {}
 }
+
+
+
+
